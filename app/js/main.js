7d95bb8b1aee2b43899ff92a2c1ec9860a13588c
Novemberman90 <alexledovit@gmail.com>
2024-09-01 08:58:17 +0300

    added cart page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function() {
 
+  /* card page */
+    $('.card-product__quantity-input').styler();
+  /* product-page */
     $('.product-tabs__top-item').on('click', function(e) {
       e.preventDefault();
       $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
@@ -28,6 +31,7 @@ $(function() {
       prevArrow:'<button type="button" class="slick-prev"><svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.20263 5.31578C7.9444 5.51267 7.57545 5.46294 7.37856 5.2047L4.44463 1.35659C4.37158 1.30823 4.26493 1.30823 4.19189 1.35659L1.25796 5.2047C1.06107 5.46294 0.692119 5.51267 0.433882 5.31578C0.175646 5.11889 0.125913 4.74994 0.322801 4.4917L3.26686 0.630317C3.30121 0.585256 3.3532 0.522894 3.42522 0.463225C3.93831 0.0380759 4.6982 0.0380759 5.2113 0.463225C5.28331 0.522894 5.33531 0.585256 5.36966 0.630317L8.31372 4.4917C8.5106 4.74994 8.46087 5.11889 8.20263 5.31578Z" fill=""/></svg></button>',
     });
 
+/* shop filters */
 
     $('.menu__list.footer-categories__list').hide();
     $('.footer-categories__title').on('click', function() {
@@ -66,6 +70,21 @@ $(function() {
     arrows: false
   });
 
+    $('.filter-price__input').ionRangeSlider({
+    type: "double",
+     prefix: "$",
+     onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+     },
+     onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+      /* $('.filter-price__from').text(data.from) - так я указываю атрибут у класса с какого эоемента нужно принимать значение */
+     }
+  });
+
+  /*  функция сокращения текста  */
     $(function() {
         let truncate = function(el) {
             let text = el.text(),
@@ -108,6 +127,8 @@ $(function() {
       
     }); */
 
+/* index */
+
     $(".star").rateYo({
     starWidth: "20px",
     normalFill: '#ececec',
@@ -138,25 +159,8 @@ $(function() {
     nextArrow: '<button type="button" class="slick-next"><svg width="23.869629" height="44.069016" viewBox="0 0 23.8696 44.069" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path id="Vector" d="M0.5 0.49C0.65 0.34 0.84 0.21 1.05 0.12C1.26 0.04 1.48 0 1.7 0C1.93 0 2.15 0.04 2.36 0.12C2.56 0.21 2.75 0.34 2.91 0.49L23.36 20.83C23.52 20.99 23.65 21.17 23.73 21.38C23.82 21.59 23.86 21.81 23.86 22.03C23.86 22.25 23.82 22.47 23.73 22.68C23.65 22.89 23.52 23.07 23.36 23.23L2.91 43.57C2.59 43.89 2.16 44.06 1.7 44.06C1.25 44.06 0.82 43.89 0.5 43.57C0.18 43.25 0 42.82 0 42.37C0 41.92 0.18 41.49 0.5 41.17L19.75 22.03L0.5 2.89C0.34 2.74 0.21 2.55 0.13 2.34C0.04 2.14 0 1.92 0 1.69C0 1.47 0.04 1.25 0.13 1.04C0.21 0.84 0.34 0.65 0.5 0.49Z" fill="#FFFFFF" fill-opacity="1.000000" fill-rule="evenodd"/></svg></button>' ,
   });
 
-
-  $('.filter-price__input').ionRangeSlider({
-    type: "double",
-     prefix: "$",
-     onStart: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-     },
-     onChange: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-      /* $('.filter-price__from').text(data.from) - так я указываю атрибут у класса с какого эоемента нужно принимать значение */
-     }
-  });
-
-
-
   
-
+/* шаблон для создания карточек */
   /*   class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector, ...classes) {
           this.src = src;
--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ function scripts() {
       'node_modules/rateyo/src/jquery.rateyo.js',
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'app/js/main.js'
     ])
     //  src ([ 'node_modules\swiper\swiper-bundle.js', 'app/js/main.js',]) 
