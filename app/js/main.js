0fa8eddc45c300db16368665dba211ce3ac92bfb
Novemberman90 <alexledovit@gmail.com>
2024-10-09 04:25:01 +0300

    added adaptive categories-man / woman and brands

--- app/js/main.js
+++ app/js/main.js
@@ -6,34 +6,34 @@ $(function() {
   });
 
     $(document).on('click', function(e) {
-    // Проверяем, что клик не произошел по меню или кнопке
-    if (!$(e.target).closest('.menu__list, .menu__btn').length) {
-      $('.menu__list').removeClass('menu__list--active');
-      $('.menu__btn').removeClass('menu__btn--active');
-      $('html').removeClass('lock');
-    }
-  });
+      // Проверяем, что клик не произошел по меню или кнопке
+      if (!$(e.target).closest('.menu__list, .menu__btn').length) {
+        $('.menu__list').removeClass('menu__list--active');
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('html').removeClass('lock');
+      }
+    });
   /* my-account */
 
-//order-details
-  $('.active-order__btn').on('click', function(e) {
-    e.preventDefault();
-    $('.order-details').show();
-    $('.my-orders').hide();
-     const breadcrumb = $(this).data('breadcrumb');
-  $('.breadcrumbs__link-curent').text(breadcrumb);
-  });
-  $('.order-details__title').on('click', function(e) {
-    e.preventDefault();
-    $('.order-details').hide();
-    $('.my-orders').show();
-     const breadcrumb = $(this).data('breadcrumb');
-  $('.breadcrumbs__link-curent').text(breadcrumb);
-  });
+  //order-details
+    $('.active-order__btn').on('click', function(e) {
+      e.preventDefault();
+      $('.order-details').show();
+      $('.my-orders').hide();
+      const breadcrumb = $(this).data('breadcrumb');
+    $('.breadcrumbs__link-curent').text(breadcrumb);
+    });
+    $('.order-details__title').on('click', function(e) {
+      e.preventDefault();
+      $('.order-details').hide();
+      $('.my-orders').show();
+      const breadcrumb = $(this).data('breadcrumb');
+    $('.breadcrumbs__link-curent').text(breadcrumb);
+    });
 
-  $('.order-details__delet-btn').on('click', function() {
-    $(this).closest('.order-details__item-cart').remove();
-  });
+    $('.order-details__delet-btn').on('click', function() {
+      $(this).closest('.order-details__item-cart').remove();
+    });
 
 
     // my-orders-tabs 
@@ -46,7 +46,7 @@ $(function() {
       $($(this).attr('href')).addClass('my-orders-tabs__content-item--active');
     });
 
-//wishlist 
+  //wishlist 
 
   function updateWishlist() {
   const wishlistItems = $('.wishlist__item');
@@ -60,59 +60,59 @@ $(function() {
     $('.my-account__fotter').hide();
     $('.account-tabs__link').removeClass('account-tabs__show-footer');
   }
-}
+  }
   $('.wishlist__delet-btn').on('click', function() {
     $(this).closest('.wishlist__item').remove();
     updateWishlist();
   });
   updateWishlist();
   
-// tabs item 
-function showMyAccontFooter(element) {
+  // tabs item 
+  function showMyAccontFooter(element) {
 
-  if(element.hasClass('account-tabs__link')&&element.hasClass('account-tabs__link--active')&&element.hasClass('account-tabs__show-footer')) {
-    $('.my-account__fotter').show();
-    } else {
-      $('.my-account__fotter').hide();
-    }
-}
+    if(element.hasClass('account-tabs__link')&&element.hasClass('account-tabs__link--active')&&element.hasClass('account-tabs__show-footer')) {
+      $('.my-account__fotter').show();
+      } else {
+        $('.my-account__fotter').hide();
+      }
+  }
 
-$('.account-tabs__link').on('click', function(e) {
-  e.preventDefault();
-  $('.account-tabs__link').removeClass('account-tabs__link--active');
-  $(this).addClass('account-tabs__link--active');
+  $('.account-tabs__link').on('click', function(e) {
+    e.preventDefault();
+    $('.account-tabs__link').removeClass('account-tabs__link--active');
+    $(this).addClass('account-tabs__link--active');
 
-  $('.account-tabs__content-item').removeClass('account-tabs__content-item--active');
-  $($(this).attr('href')).addClass('account-tabs__content-item--active');
+    $('.account-tabs__content-item').removeClass('account-tabs__content-item--active');
+    $($(this).attr('href')).addClass('account-tabs__content-item--active');
 
-  const breadcrumb = $(this).data('breadcrumb');
-  $('.breadcrumbs__link-curent').text(breadcrumb);
+    const breadcrumb = $(this).data('breadcrumb');
+    $('.breadcrumbs__link-curent').text(breadcrumb);
 
-  
+    
 
-  let clickedElement = $(this);
-  showMyAccontFooter(clickedElement);
-});
- showMyAccontFooter($('.account-tabs__link.account-tabs__link--active'));
+    let clickedElement = $(this);
+    showMyAccontFooter(clickedElement);
+  });
+  showMyAccontFooter($('.account-tabs__link.account-tabs__link--active'));
 
 
 
-// My Info change section
-$('.personal-adress__title-link').on('click', function(e) {
-  e.preventDefault();
-  $(this).closest('.personal-info').hide().siblings().show();
+  // My Info change section
+  $('.personal-adress__title-link').on('click', function(e) {
+    e.preventDefault();
+    $(this).closest('.personal-info').hide().siblings().show();
 
-  const breadcrumb = $(this).data('breadcrumb');
-  $('.breadcrumbs__link-curent').text(breadcrumb);
-});
-$('.delivery-person__btn').on('click', function() {
-  $(this).closest('.personal-info').hide().siblings().show();
-  
-  const breadcrumb = $(this).data('breadcrumb');
-  $('.breadcrumbs__link-curent').text(breadcrumb);
-});
+    const breadcrumb = $(this).data('breadcrumb');
+    $('.breadcrumbs__link-curent').text(breadcrumb);
+  });
+  $('.delivery-person__btn').on('click', function() {
+    $(this).closest('.personal-info').hide().siblings().show();
+    
+    const breadcrumb = $(this).data('breadcrumb');
+    $('.breadcrumbs__link-curent').text(breadcrumb);
+  });
 
-// personal-adress
+  // personal-adress
     $(function (params) {
       let currentlyEditing = null;
       $('.personal-info__btn').on('click', function() {
@@ -187,24 +187,24 @@ $('.delivery-person__btn').on('click', function() {
   });
 
   /* checkout page */
-/* security-code */
-$('.payment-method__security-code-open').hide();
-  $('.payment-method__security-code-box').on('click', function() {
-    const passwordField = $('#security-code');
-    const passwordFieldType = passwordField.attr('type');
+  /* security-code */
+  $('.payment-method__security-code-open').hide();
+    $('.payment-method__security-code-box').on('click', function() {
+      const passwordField = $('#security-code');
+      const passwordFieldType = passwordField.attr('type');
 
-     // Если тип поля пароль, меняем его на текст для показа
-    if (passwordFieldType === 'password') {
-      passwordField.attr('type', 'text');
-      $('.payment-method__security-code-close').hide();
-      $('.payment-method__security-code-open').show();
-       // Меняем иконку на "глаз с чертой"
-    } else {
-      passwordField.attr('type', 'password');
-      $('.payment-method__security-code-open').hide();
-      $('.payment-method__security-code-close').show();
-       // Меняем иконку обратно на обычный глаз
-    }
+      // Если тип поля пароль, меняем его на текст для показа
+      if (passwordFieldType === 'password') {
+        passwordField.attr('type', 'text');
+        $('.payment-method__security-code-close').hide();
+        $('.payment-method__security-code-open').show();
+        // Меняем иконку на "глаз с чертой"
+      } else {
+        passwordField.attr('type', 'password');
+        $('.payment-method__security-code-open').hide();
+        $('.payment-method__security-code-close').show();
+        // Меняем иконку обратно на обычный глаз
+      }
   });
 
   // payment-method item
@@ -226,8 +226,8 @@ $('.payment-method__security-code-open').hide();
   /* card page */
   //Quantity
 
-$(function() {
-  function updateSubtotal(card) {
+  $(function() {
+    function updateSubtotal(card) {
      let $card = $(card);
      let price =  parseFloat($card.find('.card-product__cost').data('cost'));
      let quantity = parseInt($card.find('.card-product__quantity-input input').val(), 10);
@@ -243,7 +243,7 @@ $(function() {
 
      // После обновления отдельной карточки, обновляем общую сумму
      updateTotal();
-  }
+   }
 
   function updateTotal() {
     let total = 0;
@@ -354,7 +354,7 @@ $(function() {
       prevArrow:'<button type="button" class="slick-prev"><svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.20263 5.31578C7.9444 5.51267 7.57545 5.46294 7.37856 5.2047L4.44463 1.35659C4.37158 1.30823 4.26493 1.30823 4.19189 1.35659L1.25796 5.2047C1.06107 5.46294 0.692119 5.51267 0.433882 5.31578C0.175646 5.11889 0.125913 4.74994 0.322801 4.4917L3.26686 0.630317C3.30121 0.585256 3.3532 0.522894 3.42522 0.463225C3.93831 0.0380759 4.6982 0.0380759 5.2113 0.463225C5.28331 0.522894 5.33531 0.585256 5.36966 0.630317L8.31372 4.4917C8.5106 4.74994 8.46087 5.11889 8.20263 5.31578Z" fill=""/></svg></button>',
     });
 
-/* shop filters */
+  /* shop filters */
 
     $('.footer-categories__list').hide();
     $('.footer-categories__title').on('click', function() {
@@ -438,19 +438,19 @@ $(function() {
          $('.feedback__item-text').truncateText();
     });
 
-/*     $(".feedback__item-text").text(function(i, text) {
+    /* $(".feedback__item-text").text(function(i, text) {
 
-      if (text.length >= 50) {
-        text = text.substring(0, 50);
-        var lastIndex = text.lastIndexOf(" ");       // позиция последнего пробела
-        text = text.substring(0, lastIndex) + '...'; // обрезаем до последнего слова
-      }
-      
-      $(this).text(text);
-      
-    }); */
+        if (text.length >= 50) {
+          text = text.substring(0, 50);
+          var lastIndex = text.lastIndexOf(" ");       // позиция последнего пробела
+          text = text.substring(0, lastIndex) + '...'; // обрезаем до последнего слова
+        }
+        
+        $(this).text(text);
+        
+      }); */
 
-/* index */
+  /* index */
 
     $(".star").rateYo({
     starWidth: "20px",
@@ -510,13 +510,15 @@ $(function() {
     slidesToScroll: 1,
     dots: true,
     fade: true,
-/*     autoplay: true,
-    autoplaySpeed: 2000, */
+    autoplay: true,
+    autoplaySpeed: 2000,
     prevArrow: '<button type="button" class="slick-prev"><svg width="23.869629" height="44.069016" viewBox="0 0 23.8696 44.069" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path id="Vector" d="M23.36 43.57C23.21 43.72 23.02 43.85 22.81 43.93C22.6 44.02 22.38 44.06 22.16 44.06C21.93 44.06 21.71 44.02 21.5 43.93C21.3 43.85 21.11 43.72 20.95 43.57L0.5 23.23C0.34 23.07 0.21 22.88 0.13 22.68C0.04 22.47 0 22.25 0 22.03C0 21.81 0.04 21.59 0.13 21.38C0.21 21.17 0.34 20.99 0.5 20.83L20.95 0.49C21.27 0.17 21.7 0 22.16 0C22.61 0 23.04 0.17 23.36 0.49C23.68 0.81 23.86 1.24 23.86 1.69C23.86 2.14 23.68 2.57 23.36 2.89L4.11 22.03L23.36 41.17C23.52 41.32 23.65 41.51 23.73 41.72C23.82 41.92 23.86 42.14 23.86 42.37C23.86 42.59 23.82 42.81 23.73 43.02C23.65 43.22 23.52 43.41 23.36 43.57Z" fill="#FFFFFF" fill-opacity="1.000000" fill-rule="evenodd"/></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="23.869629" height="44.069016" viewBox="0 0 23.8696 44.069" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path id="Vector" d="M0.5 0.49C0.65 0.34 0.84 0.21 1.05 0.12C1.26 0.04 1.48 0 1.7 0C1.93 0 2.15 0.04 2.36 0.12C2.56 0.21 2.75 0.34 2.91 0.49L23.36 20.83C23.52 20.99 23.65 21.17 23.73 21.38C23.82 21.59 23.86 21.81 23.86 22.03C23.86 22.25 23.82 22.47 23.73 22.68C23.65 22.89 23.52 23.07 23.36 23.23L2.91 43.57C2.59 43.89 2.16 44.06 1.7 44.06C1.25 44.06 0.82 43.89 0.5 43.57C0.18 43.25 0 42.82 0 42.37C0 41.92 0.18 41.49 0.5 41.17L19.75 22.03L0.5 2.89C0.34 2.74 0.21 2.55 0.13 2.34C0.04 2.14 0 1.92 0 1.69C0 1.47 0.04 1.25 0.13 1.04C0.21 0.84 0.34 0.65 0.5 0.49Z" fill="#FFFFFF" fill-opacity="1.000000" fill-rule="evenodd"/></svg></button>' ,
   });
-
   
+  
+});
+
 /* шаблон для создания карточек */
   /*   class MenuCard {
         constructor(src, alt, title, descr, price, parentSelector, ...classes) {
@@ -565,6 +567,3 @@ $(function() {
         '.shop-content__inner',
         'product__item',
     ).render(); */
-
-});
-
